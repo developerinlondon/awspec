69900919c2cf3c8341696fc4d6b44ab1e8865004
Nayeem Syed <developerinlondon@gmail.com>
2020-08-07 16:14:53 +0100

    DAAP-2142 testing glue

--- lib/awspec/type/glue.rb
+++ lib/awspec/type/glue.rb
@@ -2,7 +2,7 @@ module Awspec::Type
   class Glue < ResourceBase
 
     def resource_via_client
-      @resource_via_client ||= find_catalog(@id)
+      @resource_via_client ||= get_catalog(id)
     end
 
     def id
@@ -11,11 +11,11 @@ module Awspec::Type
 
     def has_catalog?(catalog)
       check_existence
-      get_catalog(id, catalog)
+      get_catalog(catalog)
     end
 
-    def has_database?(databasename)
-      get_database(id, databasename)
+    def has_database?(database)
+      get_database(id, database)
     end
 
     def has_table?(database:, table:)
