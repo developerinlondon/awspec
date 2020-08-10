56cd85d15474fd1fa8d04e2e97c0a9d7804aec48
Nayeem Syed <developerinlondon@gmail.com>
2020-08-10 13:09:47 +0100

    DAAP-2142 testing glue

--- lib/awspec/matcher/have_get_tables_permission.rb
+++ lib/awspec/matcher/have_get_tables_permission.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define :have_get_tables_permission do
   match do |glue|
-    glue.has_get_table_permission?(database: @database)
+    glue.has_get_tables_permission?(database: @database)
   end
 
   chain :for_database do |database|
