b42fabdb6a6c1934c4011243352acfdd6ba089a9
Nayeem Syed <developerinlondon@gmail.com>
2020-08-11 13:35:34 +0100

    DAAP-2142 testing glue

--- lib/awspec/matcher/have_create_table_permission.rb
+++ lib/awspec/matcher/have_create_table_permission.rb
@@ -1,5 +1,6 @@
 RSpec::Matchers.define :have_create_table_permission do
   match do |glue|
+    puts "inside matcher: d-#{@database} t-#{@table}"
     glue.has_create_table_permission?(database: @database, table: @table, description: @description)
   end
 
