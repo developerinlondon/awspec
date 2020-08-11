fbab1a90d0a2b34f26aef094da20b72cd06ca7ed
Nayeem Syed <developerinlondon@gmail.com>
2020-08-11 13:27:08 +0100

    DAAP-2142 testing glue

--- lib/awspec/matcher/have_delete_table_permission.rb
+++ lib/awspec/matcher/have_delete_table_permission.rb
@@ -1,5 +1,6 @@
 RSpec::Matchers.define :have_delete_table_permission do
   match do |glue|
+    puts "inside matcher: d-#{@database} t-#{@table}"
     glue.has_delete_table_permission?(database: @database, table: @table)
   end
 
