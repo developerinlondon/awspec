705a14e89225f2e834f06da6b62aba08732c8629
Nayeem Syed <nayee.syed@bluelab.edfenergy.com>
2020-11-03 02:36:54 +0000

    DAAP-2369 testing

--- lib/awspec/type/kms.rb
+++ lib/awspec/type/kms.rb
@@ -12,6 +12,14 @@ module Awspec::Type
       resource_via_client.enabled
     end
 
+    def has_key_policy_with_correct_iams?(policy_name, document = nil)
+      res = kms_client.get_key_policy(key_id: id, policy_name: policy_name)
+      policy = JSON.parse(URI.decode(res.policy))
+      print("#{policy}")
+      return JSON.parse(URI.decode(res.policy)) == JSON.parse(document) if document
+      res
+    end
+
     def has_key_policy?(policy_name, document = nil)
       res = kms_client.get_key_policy(key_id: id, policy_name: policy_name)
       return JSON.parse(URI.decode(res.policy)) == JSON.parse(document) if document
