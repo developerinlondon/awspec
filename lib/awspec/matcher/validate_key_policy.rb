9d2563eb2a9502850a2311ad5fa4687599ce01e3
Nayeem Syed <Nayeem.Syed@edfenergy.com>
2020-11-10 10:14:57 +0000

    DAAP-2369 added awspec for testing kms keys policies (#11)
    Co-authored-by: Nayeem Syed <nayee.syed@bluelab.edfenergy.com>

---
+++ lib/awspec/matcher/validate_key_policy.rb
@@ -0,0 +1,10 @@
+RSpec::Matchers.define :have_valid_key_policy do |policy_name|
+  match do |key_type|
+    key_type.validate_key_policy?(@iam_regex)
+  end
+
+  chain :with_iam_regex do |iam_regex|
+    @iam_regex = iam_regex
+  end
+end
+  
--- lib/awspec/type/kms.rb
+++ lib/awspec/type/kms.rb
@@ -12,6 +12,28 @@ module Awspec::Type
       resource_via_client.enabled
     end
 
+    def validate_key_policy?(iam_regex)
+      retval = true
+      res = kms_client.get_key_policy(key_id: id, policy_name: 'default')
+      policy = JSON.parse(URI.decode(res.policy))
+      policy["Statement"].each do |statement|
+        if statement["Principal"]["AWS"].kind_of?(Array) then
+          statement["Principal"]["AWS"].each do |principal|
+            unless principal.match(iam_regex) then
+              retval = retval && false
+              print "Invalid Principal #{principal} in Sid #{statement['Sid']}\n"
+            end
+          end
+        else
+          unless statement["Principal"]["AWS"].match(iam_regex) then
+            retval = retval && false
+            print "Invalid Principal #{statement["Principal"]["AWS"]} in Sid #{statement['Sid']}\n"
+          end
+        end
+      end
+      return retval
+    end
+
     def has_key_policy?(policy_name, document = nil)
       res = kms_client.get_key_policy(key_id: id, policy_name: policy_name)
       return JSON.parse(URI.decode(res.policy)) == JSON.parse(document) if document
