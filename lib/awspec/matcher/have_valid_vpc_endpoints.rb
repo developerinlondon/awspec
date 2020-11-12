20de7ad8e6e1159e24843697de397fc14ffc539e
Nayeem Syed <nayee.syed@bluelab.edfenergy.com>
2020-11-12 14:25:21 +0000

    Added vpc endpoint matcher

--- lib/awspec/matcher/have_valid_vpc_endpoints.rb
+++ lib/awspec/matcher/have_valid_vpc_endpoints.rb
@@ -1,7 +1,7 @@
 RSpec::Matchers.define :have_valid_vpc_endpoints do |vpc_id|
     print "inside matcher #{vpc_id}"
     match do |vpc|
-      vpc.has_valid_vpc_endpoint?
+      vpc.has_valid_vpc_endpoints?
     end
 
   end
