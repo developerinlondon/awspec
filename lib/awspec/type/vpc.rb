8bd2dd2bf13f9fe5f002dec3b19e4c6fb6b285af
Nayeem Syed <nayee.syed@bluelab.edfenergy.com>
2020-11-12 15:01:57 +0000

    Added vpc endpoint matcher

--- lib/awspec/type/vpc.rb
+++ lib/awspec/type/vpc.rb
@@ -62,7 +62,7 @@ module Awspec::Type
     def has_valid_vpc_endpoints?()
       res = select_vpc_endpoints()
       retval = true
-      endpoints = JSON.parse(URI.decode(res.vpc_endpoints))
+      endpoints = JSON.parse(res.vpc_endpoints)
       endpoints.each do |vpc_endpoint|
         print "#{vpc_endpoint}\n\n"
         # if vpc_endpoint["Principal"]["AWS"].kind_of?(Array) then
